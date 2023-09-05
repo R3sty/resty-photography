03246f5f87f860df0dc2dc5b4806172026937cd7
Resty <54195157+R3sty@users.noreply.github.com>
2023-09-05 15:56:43 +0900

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Resty Iral",
+  description: "Resty Iral's stunning photography prints. High-quality, captivating moments captured to adorn your space",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
