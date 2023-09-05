63d8e2bdddf26a2420246da0eb94b96b4dbbd7e0
Resty <54195157+R3sty@users.noreply.github.com>
2023-09-05 17:15:45 +0900

    set up sanity schema

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,13 +1,14 @@
 import { client } from "@/sanity/lib/client"
 import { groq } from "next-sanity"
-
+import { seedSanityData } from "@/lib/seed"
 import { SanityProduct } from "@/config/inventory"
 import { ProductGallery } from "@/components/product-gallery"
 import { ProductInfo } from "@/components/product-info"
 
 interface Props {}
 
-export default function Page() {
+export default async function Page() {
+  await seedSanityData()
   return (
     <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
       <div className="mx-auto max-w-2xl lg:max-w-none">
