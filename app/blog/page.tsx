2afb2a1956f4ef81cb0772c257f72900ecf8df35
Resty <54195157+R3sty@users.noreply.github.com>
2023-09-30 20:07:05 +0900

    started blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,71 @@
-import { client } from "@/sanity/lib/client"
-import { groq } from "next-sanity"
+"use client"
 
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
+import { useEffect, useState } from "react";
+import { NextSeo } from 'next-seo';
+import { client } from "@/sanity/lib/client";
+import ReactMarkdown from 'react-markdown';
 
+type BlogPostProps = {
+  slug: string | undefined;
+};
 
-interface Props {}
+const BlogPost: React.FC<BlogPostProps> = ({ slug }) => {
+  const [post, setPost] = useState(null);
 
-export default function Blog() {
+  useEffect(() => {
+    async function fetchPostData() {
+      const data = await getPostData(slug);
+      setPost(data);
+    }
+
+    fetchPostData();
+  }, [slug]);
+
+  if (!post) return <div>Loading...</div>;
+
   return (
-    <div>
-      <h1>Blog works!</h1>
+    <div className="max-w-4xl mx-auto p-6">
+      <NextSeo
+        title={post.title}
+        description={post.excerpt}
+        openGraph={{
+          title: post.title,
+          description: post.excerpt,
+          images: [
+            {
+              url: post.thumbnail.asset.url,
+              alt: post.thumbnail.alt
+            }
+          ]
+        }}
+      />
+      <h1 className="text-4xl font-semibold mb-4">{post.title}</h1>
+      <div className="mb-4">
+        <span className="text-gray-600">Published on {new Date(post.publishedAt).toDateString()}</span>
+        <span className="text-gray-600 mx-2">|</span>
+        <span className="text-gray-600">By {post.author}</span>
+      </div>
+      <img src={post.thumbnail.asset.url} alt={post.thumbnail.alt} className="w-full h-auto mb-6 rounded-md shadow-md" />
+      <ReactMarkdown className="prose">{post.content}</ReactMarkdown>
+      <p className="mt-6 text-gray-600">{post.excerpt}</p>
     </div>
-  )
+  );
 }
+
+async function getPostData(slug: string | undefined) {
+  return await client.fetch(`*[_type == "blogPost" && slug.current == "${slug}"][0]{
+    title,
+    publishedAt,
+    author,
+    "thumbnail": {
+      asset->{
+        url
+      },
+      alt
+    },
+    content,
+    excerpt
+  }`);
+}
+
+export default BlogPost;
