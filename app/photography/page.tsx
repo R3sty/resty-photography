8676e8df045885bbd6a9afd007f0363243902429
Resty <54195157+R3sty@users.noreply.github.com>
2024-01-08 14:38:19 +0900

    refactored photography page

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -6,10 +6,10 @@ const Photography: React.FC = () => {
     <div>
       <main className="max-w-full px-2 md:px-4 lg:px-6">
         <section>
-          <Banner imgSrc="https://storage.googleapis.com/photo-portfolio/20221105-R0012519.jpg" title="Landscapes" linkHref="/photography/landscapes" alt="peak of Mount Ioudake"/>
+          <Banner imgSrc="https://storage.googleapis.com/photo-portfolio/20221105-R0012519.jpg" title="Landscapes" linkHref="/photography/landscapes" alt="peak of Mount Ioudake" />
         </section>
         <section className="mt-2">
-          <Banner imgSrc="https://storage.googleapis.com/photo-portfolio/molly-porter-K7oqWgDJQ6E-unsplash.jpg" title="Street Photography" linkHref="/street-photography" alt="street"/>
+          <Banner imgSrc="https://storage.googleapis.com/photo-portfolio/molly-porter-K7oqWgDJQ6E-unsplash.jpg" title="Street Photography" linkHref="/street-photography" alt="street" />
         </section>
       </main>
     </div>
